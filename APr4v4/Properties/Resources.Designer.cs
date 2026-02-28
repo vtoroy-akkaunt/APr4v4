b98195317ed68d3f90e6dbb0569c2c5305e763c5
234912 <234912@invalid>
2026-02-28 19:37:33 +0300

    Добавить 3-ю формулу

--- APr4v4/Properties/Resources.Designer.cs
+++ APr4v4/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace APr4v4.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Image3 {
+            get {
+                object obj = ResourceManager.GetObject("Image3", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
