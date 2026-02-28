725a9a16461068c31cb677f89b2f28ead4682c89
234912 <234912@invalid>
2026-02-28 18:27:27 +0300

    Вторая страница

--- APr4v4/2.xaml.cs
+++ APr4v4/2.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +20,29 @@ namespace APr4v4 {
     public partial class _2 : Page {
         public _2() {
             InitializeComponent();
+        }
+
+        private void to_calc_Click(object sender, RoutedEventArgs e) {
+            Func<double, double> f = fx_shx.IsChecked.GetValueOrDefault()
+                ? new Func<double, double>(x => Math.Sinh(x))
+                : (fx_x2.IsChecked.GetValueOrDefault()
+                    ? new Func<double, double>(x => x * x)
+                    : new Func<double, double>(x => Math.Pow(Math.E, x))
+                );
+            try {
+                var x_ = Convert.ToDouble(x.Text);
+                var y_ = Convert.ToDouble(y.Text);
+                var w_ = x_ == y_ ? 0.5 + Math.Pow(y_ + f(x_), 3) : (
+                    Math.Atan(f(x_)) + Math.Pow(f(x_) - y_, 3) * (x_ > y_ ? 1 : -1) // очевидно
+                );
+                res.Text = w_.ToString();
+            } catch (Exception) {
+                App.show_invalid_input();
+            }
+        }
+
+        private void to_clear_Click(object sender, RoutedEventArgs e) {
+            x.Clear(); y.Clear(); res.Clear();
         }
     }
 }
--- APr4v4/Properties/Resources.Designer.cs
+++ APr4v4/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace APr4v4.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Image2 {
+            get {
+                object obj = ResourceManager.GetObject("Image2", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
